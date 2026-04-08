4f0fd0c13ed4d1f400483e67e8eedbd8518d6877
재민 <silvull2t@naver.com>
2026-04-08 14:21:48 +0900

    init: first commit

---
+++ src/main/java/jpa/basic/alldayprojectcommerce/AlldayProjectCommerceApplication.java
@@ -0,0 +1,13 @@
+package jpa.basic.alldayprojectcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlldayProjectCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlldayProjectCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa/basic/alldayprojectcommerce/AlldayProjectCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.basic.alldayprojectcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlldayProjectCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
