915d6ca4b0ed8a6d48c4c973003eebbf56331db8
hyun2ee2 <Hongkobi04@gmail.com>
2026-04-10 17:38:22 +0900

    chore: Product Entity 필드값 설정 및 @Build, @EntityListeners 어노테이션 제거

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Product.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Product.java
@@ -1,36 +1,44 @@
 package jpa.basic.alldayprojectcommerce.domain.product.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jpa.basic.alldayprojectcommerce.domain.BaseEntity;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @Table(name = "products")
-@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Column(nullable = false, length = 100)
     private String productName;
 
+    @NotBlank
+    @Column(nullable = false)
     private Long price;
+
+    @NotBlank
+    @Column(nullable = false)
     private Integer stock;
+
+    @NotBlank
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
+    @NotBlank
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private Status status;
 
+    @NotBlank
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 100)
     private Category category;
 }
