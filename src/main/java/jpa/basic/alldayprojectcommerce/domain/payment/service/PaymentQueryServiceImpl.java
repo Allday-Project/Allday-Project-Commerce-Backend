68e49e0f80dfed90ae0c21bbf0df367647290b57
Max-1012 <113974454+Max-1012@users.noreply.github.com>
2026-04-10 17:50:50 +0900

    Feat: Payment Entity and 3 Layer Architecture Package Base classes (#7)
    * Feat: Payment Entity and 3 Layer Architecture Package Base classes
    
    * Fix: API 경로 수정
    
    * Fix:Order 엔티티와 연관관계 삭제 후 fk만 사용하도록 변경"
    
    * Fix: Payment, Payment Controller 오류 수정
    
    * Refactor: Payment 컬럼 length 추가
    
    * Refactor : 자잘한 오류 수정

---
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/payment/service/PaymentQueryServiceImpl.java
@@ -0,0 +1,7 @@
+package jpa.basic.alldayprojectcommerce.domain.payment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentQueryServiceImpl implements PaymentQueryService {
+}
