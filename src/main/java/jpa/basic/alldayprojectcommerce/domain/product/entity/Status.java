631ba1836b912f0e20486ff7e61600d12bb7954e
hyun2ee2 <Hongkobi04@gmail.com>
2026-04-10 17:05:38 +0900

    chore: BaseEntity를 상품 Entity에 상속, Status Enum 품절 부분 OUT_OF_STOCK -> SOLD_OUT으로 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Status.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Status.java
@@ -2,7 +2,7 @@ package jpa.basic.alldayprojectcommerce.domain.product.entity;
 
 public enum Status {
     ON_SALE("판매중"),
-    OUT_OF_STOCK("품절"),
+    SOLD_OUT("품절"),
     DISCONTINUED("단종");
 
     private final String displayName;
