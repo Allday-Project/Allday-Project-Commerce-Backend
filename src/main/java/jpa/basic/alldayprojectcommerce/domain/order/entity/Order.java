d8551231bd0393b08ae451070a5d208a776bb05f
재민 <silvull2t@naver.com>
2026-04-11 14:10:22 +0900

    feat: Order 엔티티와 3-Layer-Architecture 틀 구현

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/Order.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/Order.java
@@ -1,4 +1,42 @@
 package jpa.basic.alldayprojectcommerce.domain.order.entity;
 
-public class Order {
+import jakarta.persistence.*;
+import jpa.basic.alldayprojectcommerce.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false, length = 100)
+    private String orderUid;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String orderNumber;
+
+    @Column(nullable = false)
+    private Long totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Builder
+    public Order(Long userId, String orderUid, String orderNumber, Long totalAmount, OrderStatus status) {
+        this.userId = userId;
+        this.orderUid = orderUid;
+        this.orderNumber = orderNumber;
+        this.totalAmount = (totalAmount == null) ? 0L : totalAmount;
+        this.status = status;
+    }
 }
---
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderStatus.java
@@ -0,0 +1,11 @@
+package jpa.basic.alldayprojectcommerce.domain.order.entity;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    DELIVERY_COMPLETED,
+    CANCELLED,
+    REFUNDED,
+    REFUND_REQUESTED,
+    CONFIRMED
+}
