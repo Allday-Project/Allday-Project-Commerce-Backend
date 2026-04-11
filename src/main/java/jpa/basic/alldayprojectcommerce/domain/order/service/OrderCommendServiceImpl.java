a17f20090db8e538528d9ac7583ba47a36f7ac3f
재민 <silvull2t@naver.com>
2026-04-11 14:10:22 +0900

    feat: Order 엔티티와 3-Layer-Architecture 틀 구현

---
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/service/OrderCommendServiceImpl.java
@@ -0,0 +1,14 @@
+package jpa.basic.alldayprojectcommerce.domain.order.service;
+
+import jpa.basic.alldayprojectcommerce.domain.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderCommendServiceImpl {
+
+    private final OrderRepository orderRepository;
+}
