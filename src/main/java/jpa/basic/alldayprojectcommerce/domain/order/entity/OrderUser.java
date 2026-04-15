b76e3ea37c18cc3a0d40734c12838750e73cb46b
재민 <silvull2t@naver.com>
2026-04-15 14:17:27 +0900

    feat: OrderUser 엔티티 개발

---
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderUser.java
@@ -0,0 +1,38 @@
+package jpa.basic.alldayprojectcommerce.domain.order.entity;
+
+import jakarta.persistence.*;
+import jpa.basic.alldayprojectcommerce.domain.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "order_users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderUser extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Builder
+    public OrderUser(Long orderId, String name, String phone, String address) {
+        this.orderId = orderId;
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+}
