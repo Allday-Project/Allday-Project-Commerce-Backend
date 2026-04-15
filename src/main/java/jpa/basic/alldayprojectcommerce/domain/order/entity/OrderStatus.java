5f6fa1fe0c5e2dc3d32eb7f58118d84848e0fee7
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-04-15 14:44:18 +0900

    feat: Order 엔티티 개발 및 3-Layer-Architecture 틀 구성 (#30)
    * feat: Order 엔티티 개발 및 3-Layer-Architecture 틀 구성

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderStatus.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderStatus.java
@@ -1,11 +1,11 @@
 package jpa.basic.alldayprojectcommerce.domain.order.entity;
 
 public enum OrderStatus {
-    PENDING,
-    PAID,
-    DELIVERY_COMPLETED,
-    CANCELLED,
-    REFUNDED,
-    REFUND_REQUESTED,
-    CONFIRMED
+    PENDING,                // 결제 대기
+    PAID,                   // 결제 완료
+    DELIVERY_COMPLETED,     // 배송 완료
+    CANCELLED,              // 주문 취소
+    REFUNDED,               // 환불 완료
+    REFUND_REQUESTED,       // 환불 요청
+    CONFIRMED               // 구매 확정
 }
