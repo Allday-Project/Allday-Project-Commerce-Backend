89f2a91c2588e42e1bc7b58dde54d2ba85cc817a
munhyerin22 <ansgpfls7942@gmail.com>
2026-04-15 12:50:13 +0900

    feat: User도메인 마이페이지 조회 및 내 정보 수정 기능구현 #22 (#27)
    * feat: User도메인 마이페이지 조회 및 내 정보 수정 기능구현
    
    * chore: AI 코드리뷰 내용 반영하여 수정

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/user/service/UserCommandService.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/user/service/UserCommandService.java
@@ -1,8 +1,12 @@
 package jpa.basic.alldayprojectcommerce.domain.user.service;
 
+import jpa.basic.alldayprojectcommerce.domain.user.dto.request.UpdatemeUserRequest;
 import jpa.basic.alldayprojectcommerce.domain.user.entity.User;
 
 public interface UserCommandService {
 
     User create(String email, String encodedPassword);
+
+    // 내 정보 수정
+    void updateProfile(Long userId, UpdatemeUserRequest request);
 }
