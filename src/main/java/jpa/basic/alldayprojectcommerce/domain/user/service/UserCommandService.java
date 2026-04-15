62e05b5701ea887667ed79961317a5c1889c35bf
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-04-15 14:59:33 +0900

    Refactor: 인증/인가에서 사용하지 않는 메서드 삭제 및 주석 추가 (#26)
    * Refactor: 인증/인가에서 사용하지 않는 메서드 삭제 및 주석 추가

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/user/service/UserCommandService.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/user/service/UserCommandService.java
@@ -1,11 +1,11 @@
 package jpa.basic.alldayprojectcommerce.domain.user.service;
 
 import jpa.basic.alldayprojectcommerce.domain.user.dto.request.UpdatemeUserRequest;
-import jpa.basic.alldayprojectcommerce.domain.user.entity.User;
 
 public interface UserCommandService {
 
-    User create(String email, String encodedPassword);
+    // 회원가입에서 사용하는 메서드입니다.
+    void create(String email, String encodedPassword);
 
     // 내 정보 수정
     void updateProfile(Long userId, UpdatemeUserRequest request);
