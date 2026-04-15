eaf847fa448ae8fe729fb4217b0666c942901fc2
DGAZA-max <name@naver.com>
2026-04-15 23:27:45 +0900

    feat: 레포지토리 클래스 이름 수정(ProductRepositoryImpl -> ProductRepository) #28

--- src/main/java/jpa/basic/alldayprojectcommerce/common/exception/ErrorCode.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/common/exception/ErrorCode.java
@@ -16,14 +16,11 @@ public enum ErrorCode {
 
     //   공통 에러 코드 (A###)
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "A001", "입력값이 올바르지 않습니다."),
-    DATABASE_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "A003", "서버가 응답하지 않습니다."),
+    DATABASE_CONNECTION_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "A002", "서버가 응답하지 않습니다."),
 
     //   사용자 관련 에러 코드 (U###)
     USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "U001", "이미 존재하는 이메일입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U002", "해당 유저는 존재하지 않습니다."),
-
-    //   파일 관련 에러 코드 (F###)
-    FILE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "F001", "파일 업로드를 실패했습니다."),
 
     //   프로필 관련 에러 코드 (P###)
     PROFILE_NOT_FOUND(HttpStatus.NOT_FOUND, "P001", "프로필을 찾을 수 없습니다."),
