3856eb49de921a318e6824abdec7510a508aaebe
Hye_rin Mun <ansgpfls7942@gmail.com>
2026-04-17 01:23:11 +0900

    feat: 비밀번호 수정 로직 분리

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/user/repository/UserRepository.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/user/repository/UserRepository.java
@@ -8,9 +8,12 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    // 회원가입 시 이메일 중복확인
     boolean existsUserByEmail(String email);
 
+    // 로그인 시 유저DB에 있는 email인지 조회
     Optional<User> findByEmail(String email);
 
+    //?????
     List<User> id(Long id);
 }
