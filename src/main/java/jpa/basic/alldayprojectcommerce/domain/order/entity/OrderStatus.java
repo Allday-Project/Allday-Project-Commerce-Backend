ddedce5b642dd88cadf137bce87c9b447fcd6bd7
soyeong <thdud001012@gmail.com>
2026-04-17 14:59:19 +0900

    Refactor : OrderStatus PAID -> COMPLETED로 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderStatus.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/entity/OrderStatus.java
@@ -2,7 +2,7 @@ package jpa.basic.alldayprojectcommerce.domain.order.entity;
 
 public enum OrderStatus {
     PENDING,                // 결제 대기
-    PAID,                   // 결제 완료 (이 상태에서만 주문 취소 가능)
+    COMPLETED,                   // 결제 완료 (이 상태에서만 주문 취소 가능)
     DELIVERY_COMPLETED,     // 배송 완료
     CANCELLED,              // 주문 취소
     REFUNDED,               // 환불 완료
