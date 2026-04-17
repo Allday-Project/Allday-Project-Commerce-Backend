9ec91eacf63df7f4f2194388297bc6ded3491dc5
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-04-17 11:25:59 +0900

    Feat: 주문 내역 목록 조회 (#45)
    * Feat: 주문 목록 조회 API
     - 본인이 주문한 목록 조회
     - 커서 기반 Slice 무한 스크롤

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/controller/OrderController.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/controller/OrderController.java
@@ -2,19 +2,18 @@ package jpa.basic.alldayprojectcommerce.domain.order.controller;
 
 import jakarta.validation.Valid;
 import jpa.basic.alldayprojectcommerce.common.ApiResponse;
+import jpa.basic.alldayprojectcommerce.common.CursorResponse;
 import jpa.basic.alldayprojectcommerce.common.security.auth.LoginUser;
 import jpa.basic.alldayprojectcommerce.common.security.auth.LoginUserInfo;
 import jpa.basic.alldayprojectcommerce.domain.order.dto.request.CreateOrderRequest;
 import jpa.basic.alldayprojectcommerce.domain.order.dto.response.CreateOrderResponse;
+import jpa.basic.alldayprojectcommerce.domain.order.dto.response.GetAllOrdersResponse;
 import jpa.basic.alldayprojectcommerce.domain.order.service.OrderCommandService;
 import jpa.basic.alldayprojectcommerce.domain.order.service.OrderQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,4 +32,17 @@ public class OrderController {
                 .body(ApiResponse.success(HttpStatus.CREATED, orderCommandService.createOrder(loginUserInfo, request)));
     }
 
+    /**
+     * 주문서 조회 - PENDING 상태 (결제 전 화면)
+     * User 정보가 null이면 프론트에서 표시
+     */
+    @GetMapping
+    public ResponseEntity<ApiResponse<CursorResponse<GetAllOrdersResponse>>> getOrder(
+            @LoginUser LoginUserInfo loginUserInfo,
+            @RequestParam(required = false) Long cursorId,
+            @RequestParam(defaultValue = "10") int size) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK, orderQueryService.getAllOrders(loginUserInfo, cursorId, size)));
+    }
 }
