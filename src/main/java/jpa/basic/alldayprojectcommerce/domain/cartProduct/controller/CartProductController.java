791ef6e69612d2b9df310a06e39e29e1d70c18d6
munhyerin22 <ansgpfls7942@gmail.com>
2026-04-17 22:48:03 +0900

    Feat/cart product create cart product#46 (#51)
    * feat: 장바구니 상품 추가(생성)기능 구현
    
    * chore: 간단한 오류 수정

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/controller/CartProductController.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/controller/CartProductController.java
@@ -1,6 +1,16 @@
 package jpa.basic.alldayprojectcommerce.domain.cartProduct.controller;
 
+import jakarta.validation.Valid;
+import jpa.basic.alldayprojectcommerce.common.ApiResponse;
+import jpa.basic.alldayprojectcommerce.common.security.auth.LoginUser;
+import jpa.basic.alldayprojectcommerce.common.security.auth.LoginUserInfo;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.dto.request.CreateCartProductRequest;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.service.CartProductCommandService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/cart")
 @RequiredArgsConstructor
 public class CartProductController {
+
+    private final CartProductCommandService cartProductCommandService;
+
+    // 장바구니 상품 추가
+    @PostMapping
+    public ResponseEntity<ApiResponse<Void>> createCartProduct(
+            @LoginUser LoginUserInfo loginUser,
+            @Valid @RequestBody CreateCartProductRequest request) {
+        cartProductCommandService.createCartProduct(loginUser.id(), request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(HttpStatus.CREATED));
+    }
 
 }
