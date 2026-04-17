968b0de672006c9e6f1303de89dbdd9d32a00737
Max-1012 <113974454+Max-1012@users.noreply.github.com>
2026-04-17 16:01:08 +0900

    Feat/payment create#18 (#23)
    * 결제 생성 기능 구현
    
    - 주문 조회에 필요한 메서드를 위해 주문 도메인에 메서드 추가
    - 에러 코드 도메인 구조별로 구역 분리
    - 실행 안되는 에러 수정을 위해 RefundStatus 내용 추가
    - 결제 생성 기능을 위한 메서드 추가, paymentUid는 일단 uuid를 이용하여 생성, 이후에 주문 도메인과 협의 후 tsid로 변경 가능성 있음
    
    * 결제 생성 기능 구현
    - 컨트롤러 추가
    - 주문 생성 기능이 없어 테스트 불가능. 실행확인까지 했습니다.
    
    * 결제 생성 기능 구현
    - 로그인 유저 검증 추가
    - 컨트롤러 PostMapping 추가
    - PaymentCommandService DI 안되는 문제 final 추가로 해결
    
    * Refactor: 팀원, AI 코드리뷰 반영하여 수정
    
    * orderUid 검증 로직 추가
    
    * Refactor: 결제 생성 시에도 order 조회 시 비관적 락 사용하여 조회. 검증 로직 추가
    
    * Refactor :결제금액 및 배송비 0원 가능 정책 적용 및 검증 로직 추가
    
    * Refactor : OrderUid 검증 패턴 변경 및 어노테이션 추가
    
    * Feat: 결제 생성 로직 완성
    - 에러 코드 수정
    - 컨트롤러 입력값 검증(@Validated) 에러 처리를 위해 GlobalExceptionHandler에 코드 추가
    -
    
    * Refactor : @Pattern에서 길이 체크 해주기 때문에 @Size 제거, @Pattern에 message 추가, 서비스 로직에서 orderUid 존재 검증 로직 제거
    
    * Refactor : OrderStatus PAID -> COMPLETED로 변경
    
    * Refactor : Payment Status에 EXPIRED 추가 및 사소한 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/repository/OrderRepository.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/repository/OrderRepository.java
@@ -1,8 +1,14 @@
 package jpa.basic.alldayprojectcommerce.domain.order.repository;
 
+import jakarta.persistence.LockModeType;
 import jpa.basic.alldayprojectcommerce.domain.order.entity.Order;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 import java.util.Optional;
 
@@ -10,4 +16,8 @@ public interface OrderRepository extends JpaRepository<Order, Long>, OrderReposi
 
     // 주문서 조회 및 상세 조회
     Optional<Order> findByOrderUid(String orderUid);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select o from Order o where o.orderUid = :orderUid")
+    Optional<Order> findByOrderUidForUpdate(@Param("orderUid") String orderUid);
 }
--- src/main/java/jpa/basic/alldayprojectcommerce/domain/payment/repository/PaymentRepository.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/payment/repository/PaymentRepository.java
@@ -1,9 +1,11 @@
 package jpa.basic.alldayprojectcommerce.domain.payment.repository;
 
 import jpa.basic.alldayprojectcommerce.domain.payment.entity.Payment;
+import jpa.basic.alldayprojectcommerce.domain.payment.entity.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    boolean existsByOrderIdAndStatus(Long id, PaymentStatus paymentStatus);
 }
