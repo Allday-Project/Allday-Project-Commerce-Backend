1bb25b20f73ec7cf74f3107ee01ad62facc74b48
Max-1012 <113974454+Max-1012@users.noreply.github.com>
2026-04-19 15:04:11 +0900

    Feat: OrderUser, ProductCommand Service 껍데기 생성 (#62)

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/product/service/ProductCommandService.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/product/service/ProductCommandService.java
@@ -1,4 +1,7 @@
 package jpa.basic.alldayprojectcommerce.domain.product.service;
 
+import jpa.basic.alldayprojectcommerce.domain.order.entity.Order;
+
 public interface ProductCommandService {
+    void decreaseStock(Order order);
 }
