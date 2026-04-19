fa16d911a1fc0a950c4719e44da81b216f323010
Max-1012 <113974454+Max-1012@users.noreply.github.com>
2026-04-19 15:04:11 +0900

    Feat: OrderUser, ProductCommand Service 껍데기 생성 (#62)

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/service/OrderUserCommandServiceImpl.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/order/service/OrderUserCommandServiceImpl.java
@@ -1,16 +1,17 @@
-package jpa.basic.alldayprojectcommerce.domain.product.service;
+package jpa.basic.alldayprojectcommerce.domain.order.service;
 
 import jakarta.transaction.Transactional;
 import jpa.basic.alldayprojectcommerce.domain.order.entity.Order;
+import jpa.basic.alldayprojectcommerce.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class ProductCommandServiceImpl implements ProductCommandService{
+public class OrderUserCommandServiceImpl implements OrderUserCommandService {
     @Override
-    public void decreaseStock(Order order) {
+    public void createSnapshot(Order order, User user) {
 
     }
 }
