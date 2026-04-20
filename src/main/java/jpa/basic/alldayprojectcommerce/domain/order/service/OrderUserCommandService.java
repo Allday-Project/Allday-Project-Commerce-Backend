bbc48de11795b94a38933c538aecca8c09839f35
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-04-20 16:38:37 +0900

    Feat: 주문서 고객정보 스냅샷 저장 및 주문 상태 변경 (#65)
    * feat: 주문서 고객정보 스냅샷 저장 및 주문 상태 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/order/service/OrderUserCommandService.java
+++
@@ -1,9 +0,0 @@
-package jpa.basic.alldayprojectcommerce.domain.order.service;
-
-import jpa.basic.alldayprojectcommerce.domain.order.entity.Order;
-import jpa.basic.alldayprojectcommerce.domain.user.entity.User;
-
-public interface OrderUserCommandService {
-
-    void createSnapshot(Order order, User user);
-}
