f8d1e65381445c4ed39e6febe008b78a6f550034
munhyerin22 <ansgpfls7942@gmail.com>
2026-04-21 15:04:47 +0900

    Feat/cart product get all cart product#53 (#55)
    * feat: User도메인 마이페이지 조회 및 내 정보 수정 기능구현
    
    * chore: AI 코드리뷰 내용 반영하여 수정
    
    * feat: 비밀번호 수정 로직 분리
    
    * chore: 사소한 변경
    
    * feat: 장바구니 상품 추가(생성)기능 구현
    
    * feat: 장바구니 전체 조회 기능 구현
    
    * fix: ProductRepository -> ProductQueryService 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/service/CartProductQueryServiceImpl.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/service/CartProductQueryServiceImpl.java
@@ -1,4 +1,68 @@
 package jpa.basic.alldayprojectcommerce.domain.cartProduct.service;
 
+import jpa.basic.alldayprojectcommerce.common.CursorResponse;
+import jpa.basic.alldayprojectcommerce.common.exception.CustomException;
+import jpa.basic.alldayprojectcommerce.common.exception.ErrorCode;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.dto.response.GetAllCartProductResponse;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.entity.CartProduct;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.repository.CartProductRepository;
+import jpa.basic.alldayprojectcommerce.domain.product.entity.Product;
+import jpa.basic.alldayprojectcommerce.domain.product.service.ProductQueryService;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CartProductQueryServiceImpl implements CartProductQueryService{
+
+    private final CartProductRepository cartProductRepository;
+    private final ProductQueryService productQueryService;
+
+    @Override
+    public CursorResponse<GetAllCartProductResponse> getAllCartProduct(
+            Long loginId, Long cursorId, int size) {
+
+        long cursor = (cursorId == null) ? Long.MAX_VALUE : cursorId;
+
+        // 1. 데이터 조회
+        List<CartProduct> cartProducts = cartProductRepository
+                .findAllByUserIdWithCursor(loginId, cursor, size);
+
+        // 2. 조회된 장바구니에 있는 모든 상품 id 추출
+        List<Long> productIds = cartProducts.stream()
+                .map(CartProduct::getProductId)
+                .toList();
+
+        // 3. ProductQueryService를 통해 상품 정보 한번에 조회
+        List<Product> products = productQueryService.findAllByIds(productIds);
+
+        Map<Long, Product> productMap = products.stream()
+                .collect(Collectors.toMap(
+                        Product::getId,
+                        p -> p,
+                        (existing, replacement) -> existing
+                ));
+        // 4. DTO 변환
+        List<GetAllCartProductResponse> responseList = cartProducts.stream()
+                .map(cartProduct -> {
+                    // 상품 정보를 레포지토리에서 조회 (혹은 fetch join 사용권장)
+                    Product product = productMap.get(cartProduct.getProductId());
+                    if(product == null) {
+                        throw new CustomException(ErrorCode.PRODUCT_NOT_FOUND);
+                    }
+                    return GetAllCartProductResponse.from(cartProduct, product);
+                }).toList();
+        // 3. 응답 생성
+        return CursorResponse.of(
+                responseList,
+                size,
+                GetAllCartProductResponse::cartProductId
+        );
+    }
 }
