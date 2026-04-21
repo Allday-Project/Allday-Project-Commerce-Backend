f6b109216cbaae094b2d2da31dd7063b670dd73a
munhyerin22 <ansgpfls7942@gmail.com>
2026-04-21 15:04:47 +0900

    Feat/cart product get all cart product#53 (#55)
    * feat: User도메인 마이페이지 조회 및 내 정보 수정 기능구현
    
    * chore: AI 코드리뷰 내용 반영하여 수정
    
    * feat: 비밀번호 수정 로직 분리
    
    * chore: 사소한 변경
    
    * feat: 장바구니 상품 추가(생성)기능 구현
    
    * feat: 장바구니 전체 조회 기능 구현
    
    * fix: ProductRepository -> ProductQueryService 변경

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/repository/CartProductRepositoryCustom.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/repository/CartProductRepositoryCustom.java
@@ -1,9 +1,13 @@
 package jpa.basic.alldayprojectcommerce.domain.cartProduct.repository;
 
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.entity.CartProduct;
 import jpa.basic.alldayprojectcommerce.domain.order.entity.Order;
 
 import java.util.List;
 
 public interface CartProductRepositoryCustom {
+
+    // 커서 기반 유저의 장바구니 목록 전체 조회
+    List<CartProduct> findAllByUserIdWithCursor(Long loginId, long cursor, int size);
 
 }
--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/repository/CartProductRepositoryCustomImpl.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/repository/CartProductRepositoryCustomImpl.java
@@ -1,5 +1,31 @@
 package jpa.basic.alldayprojectcommerce.domain.cartProduct.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.entity.CartProduct;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static jpa.basic.alldayprojectcommerce.domain.cartProduct.entity.QCartProduct.cartProduct;
+import static jpa.basic.alldayprojectcommerce.domain.product.entity.QProduct.product;
+
+@RequiredArgsConstructor
 public class CartProductRepositoryCustomImpl implements CartProductRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<CartProduct> findAllByUserIdWithCursor(Long userId, long cursorId, int size) {
+        return queryFactory
+                .selectFrom(cartProduct)
+                .join(product).on(cartProduct.productId.eq(product.id))
+                .where(
+                        cartProduct.userId.eq(userId),
+                        cartProduct.id.lt(cursorId) // cursorId 보다 작은 ID (최신순 페이징)
+                )
+                .orderBy(cartProduct.id.desc())
+                .limit(size)
+                .fetch();
+    }
 
 }
