a6f6949757fe96fd4e69216c3b609aa915f24ee7
DGAZA-max <name@naver.com>
2026-04-21 11:46:11 +0900

    feat: 재고 검증 로직 추가

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Product.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/product/entity/Product.java
@@ -57,9 +57,7 @@ public class Product extends BaseEntity {
     // 재고 차감 로직
     public void decreaseStock(int quantity) {
         validQuantity(quantity);
-        if (this.stock < quantity) {
-            throw new CustomException(ErrorCode.PRODUCT_OUT_OF_STOCK);
-        }
+        verifyStock(quantity);
         this.stock -= quantity;
         closeSales();
     }
@@ -71,8 +69,15 @@ public class Product extends BaseEntity {
         resumeSales();
     }
 
+    // 입력 수량이 재고보다 클 때 에러 날림
+    public void verifyStock(int quantity) {
+        if (this.stock < quantity) {
+            throw new CustomException(ErrorCode.PRODUCT_OUT_OF_STOCK);
+        }
+    }
+
     // 재고가 null 이거나 입력 수량이 0보다 작거나 같을 때 에러 날림
-    private void validQuantity(int quantity) {
+    public void validQuantity(int quantity) {
         if (quantity <= 0) {
             throw new CustomException(ErrorCode.INVALID_INPUT_VALUE);
         }
