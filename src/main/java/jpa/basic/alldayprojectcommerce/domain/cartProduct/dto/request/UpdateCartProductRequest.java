d1dee9d4fe692031727fac59018ee9dda528b264
munhyerin22 <ansgpfls7942@gmail.com>
2026-04-21 15:52:20 +0900

    Feat/cart product update quantity#56 (#57)
    * feat: 장바구니 상품 수량 변경 기능 구현

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/dto/request/UpdateCartProductRequest.java
+++
@@ -1,4 +0,0 @@
-package jpa.basic.alldayprojectcommerce.domain.cartProduct.dto.request;
-
-public record UpdateCartProductRequest() {
-}
--- src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/service/CartProductCommandServiceImpl.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/cartProduct/service/CartProductCommandServiceImpl.java
@@ -3,11 +3,13 @@ package jpa.basic.alldayprojectcommerce.domain.cartProduct.service;
 import jpa.basic.alldayprojectcommerce.common.exception.CustomException;
 import jpa.basic.alldayprojectcommerce.common.exception.ErrorCode;
 import jpa.basic.alldayprojectcommerce.domain.cartProduct.dto.request.CreateCartProductRequest;
+import jpa.basic.alldayprojectcommerce.domain.cartProduct.dto.request.UpdateQuantityRequest;
 import jpa.basic.alldayprojectcommerce.domain.cartProduct.entity.CartProduct;
 import jpa.basic.alldayprojectcommerce.domain.cartProduct.repository.CartProductRepository;
 import jpa.basic.alldayprojectcommerce.domain.product.entity.Product;
 import jpa.basic.alldayprojectcommerce.domain.product.entity.ProductStatus;
 import jpa.basic.alldayprojectcommerce.domain.product.repository.ProductRepository;
+import jpa.basic.alldayprojectcommerce.domain.product.service.ProductQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,15 +19,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class CartProductCommandServiceImpl implements CartProductCommandService {
 
-    private final ProductRepository productRepository;
+    private final ProductQueryService productQueryService;
     private final CartProductRepository cartProductRepository;
 
     // 장바구니 상품 추가
     @Override
     public void createCartProduct(Long userId, CreateCartProductRequest request) {
         // 상품 존재 여부 검증
-        Product product = productRepository.findById(request.productId())
-                .orElseThrow(() -> new CustomException(ErrorCode.PRODUCT_NOT_FOUND));
+        Product product = productQueryService.getByProductId(request.productId());
 
         // 판매 상태 검증
         if (product.getStatus() != ProductStatus.ON_SALE) {
@@ -53,5 +54,29 @@ public class CartProductCommandServiceImpl implements CartProductCommandService 
                                         .build()
                         )
                 );
+    }
+
+    // 장바구니 상품 수량 업데이트
+    @Override
+    public void updateQuantity(
+            Long userId, Long cartProductId, UpdateQuantityRequest request) {
+
+        // 장바구니 상품 유무 검증
+        CartProduct cartProduct = cartProductRepository.findById(cartProductId)
+                .orElseThrow(() -> new CustomException(ErrorCode.CARTPRODUCT_NOT_FOUND));
+
+        // 해당 장바구니 상품에 접근 권한 여부 검증
+        if (!cartProduct.getUserId().equals(userId)) {
+            throw new CustomException(ErrorCode.AUTH_FORBIDDEN_ACCESS);
+        }
+
+        // 장바구니에 담긴 상품의 존재 여부 검증
+        Product product = productQueryService.getByProductId(cartProduct.getProductId());
+
+        // 재고 확인
+        product.checkAvailability(request.quantity());
+
+        // @Transactional -> dirty checking으로 자동저장
+        cartProduct.updateQuantity(request.quantity());
     }
 }
