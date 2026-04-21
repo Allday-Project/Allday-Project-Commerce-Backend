b1e4917612ec07a6aed719ba1e3e2e6a53fe4230
재민 <145990859+CheatIsKey@users.noreply.github.com>
2026-04-21 16:22:35 +0900

    Feat: 1시간마다 Redis ZSet 데이터를 DB에 저장 (#75)
    * Feat: 1시간마다 Redis ZSet 데이터를 DB에 저장
     - Redis가 가지고 있는 데이터를 DB에 주기적으로 백업하는 Write-back 전략
     - Redis는 오늘 누적 전체가 담겨있어서 DB에 덮어쓰기
     - 스케쥴러를 활용해 정해진 시간마다 저장하도록 수행

--- src/main/java/jpa/basic/alldayprojectcommerce/domain/keyword/repository/SearchKeywordRepository.java
+++ src/main/java/jpa/basic/alldayprojectcommerce/domain/keyword/repository/SearchKeywordRepository.java
@@ -3,7 +3,10 @@ package jpa.basic.alldayprojectcommerce.domain.keyword.repository;
 import jpa.basic.alldayprojectcommerce.domain.keyword.entity.SearchKeyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface SearchKeywordRepository extends JpaRepository<SearchKeyword, Long> {
 
-
+    Optional<SearchKeyword> findByKeywordAndSearchDate(String keyword, LocalDate searchDate);
 }
